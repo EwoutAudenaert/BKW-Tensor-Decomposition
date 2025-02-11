8626fbad33764c33e6653dcaeabc0e9822a599f9
Jip Helsen <jip.helsen@student.kuleuven.be>
2025-02-11 22:29:46 +0100

    algoritme should be done except for differential step*

--- stap5.py
+++ stap5.py
@@ -5,7 +5,7 @@ def find_decomposition(results):
     
     for diag_matrix, eigenvectors in results:
         factor = np.prod(np.diag(diag_matrix))  
-        #in case of confusion @ is mulitply
+        #in case of confusion, @ is mulitply
         A_i = eigenvectors @ diag_matrix @ np.linalg.inv(eigenvectors)
         decomposition.append(factor,A_i)
     return decomposition
