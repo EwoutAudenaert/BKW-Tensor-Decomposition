74ea5fcf547fc594f83c5a32cf8dc1605a08f6b9
Jip Helsen <jip@apx_managed.vanilla>
2025-02-11 22:28:27 +0100

    algo should be done

--- stap4.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def get_factor_matrices(matrix):
-    pass 
---
+++ stap5.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def find_decomposition(results):
+    decomposition = []  
+    
+    for diag_matrix, eigenvectors in results:
+        factor = np.prod(np.diag(diag_matrix))  
+        #in case of confusion @ is mulitply
+        A_i = eigenvectors @ diag_matrix @ np.linalg.inv(eigenvectors)
+        decomposition.append(factor,A_i)
+    return decomposition
