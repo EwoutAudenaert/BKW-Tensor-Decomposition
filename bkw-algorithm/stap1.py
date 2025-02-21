fa0da7a3e32732e9acea535b04cacf65639fb766
Jip Helsen <jip.helsen@student.kuleuven.be>
2025-02-21 13:25:49 +0100

    added function to find matrix pencil aka step 1

--- bkw-algorithm/stap1.py
+++ bkw-algorithm/stap1.py
@@ -1,2 +1,7 @@
+import numpy as np
 # Stap 1 van het algoritme
 # We berekenen hier de afgeleide
+def find_derivative(tensor):
+    dim = len(tensor) #cubic -> ok :)
+    zeros = np.zeros(dim)
+    
--- pencil-based-algorithm/stap1.py
+++ pencil-based-algorithm/stap1.py
@@ -1,4 +1,10 @@
+import numpy as np
+
 def find_mode_subspaces(tensor):
-    fst_pencil = tensor[0]
-    snd_pencil = tensor[1]
-    
+    dim = len(tensor) # fine since the tensor is cubic
+    v1 = np.random.uniform(-1, 1, dim)
+    v2 = np.random.uniform(-1, 1, dim)
+    v1 /= np.linalg.norm(v1)
+    v2 /= np.linalg.norm(v2)
+    pencil = np.tensordot(tensor,  matrix = np.column_stack((v1, v2)),axes=(0, 0))
+    return pencil
