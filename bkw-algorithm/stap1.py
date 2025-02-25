568cabae24db926c4c0975ad54a9dc49331b2bc6
Jip Helsen <jip.helsen@student.kuleuven.be>
2025-02-25 19:47:44 +0100

    just need to double check the ttm

--- bkw-algorithm/stap1.py
+++ bkw-algorithm/stap1.py
@@ -6,34 +6,11 @@ from library import ttm
 
 # Stap 1 van het algoritme
 # We berekenen hier de afgeleide
-"""
-def find_derivative(tensor):
-    dim = len(tensor) #cubic -> ok :)
-    zeros = np.zeros(dim)
-    [mode1,mode2,mode3] = [ttm(tensor,zeros,i).reshape(-1) for i in range(1,4)] # tenors already flattened
-    return np.hstack((mode1, mode2, mode3))
-"""
+
 def unit_matrix(i, j, shape):
     M = np.zeros((shape,shape))
     M[i, j] = 1
     return M
-
-def find_derivative(tensor):
-    dim = tensor.shape[0] #cubic -> ok :)
-    matrix=[]
-    for k in range(1,4):
-        for i in range(0,dim):
-            for j in range(0,dim):
-                u = unit_matrix(i,j,dim)
-                matrix.append(np.vectorize(ttm(tensor,u,k)))
-    
-    return matrix.T
-
-tensor =np.array([
-    [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-    [[10, 11, 12], [13, 14, 15], [16, 17, 18]],
-    [[19, 20, 21], [22, 23, 24], [25, 26, 27]]
-])
 
 def print_frontal_slices(tensor):
     if len(tensor.shape) != 3:
@@ -44,6 +21,31 @@ def print_frontal_slices(tensor):
     for k in range(num_slices):
         print(f"Frontal Slice {k + 1}:\n", tensor[:, :, k], "\n")
 
-tensor = np.array([[[1,2],[2,1]],[[3,0],[4,3]]])
+def vectorize_tensor(tensor):
+    frontal_slices = [tensor[:, :, k].flatten(order='F') for k in range(tensor.shape[0])]
+    return np.concatenate(frontal_slices)
 
-print_frontal_slices(ttm(tensor,unit_matrix(0,0,2),1))
+def find_derivative(tensor):
+    dim = tensor.shape[0] #cubic -> ok :)
+    matrix=[]
+    for k in range(1,4):
+        for i in range(0,dim):
+            for j in range(0,dim):
+                u = unit_matrix(i,j,dim)
+                print("i=",i,"j=",j,"mode=",k)
+                matrix.append(vectorize_tensor(ttm(tensor,u,k)))
+
+    return matrix
+
+"""
+tensor =np.array([
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+    [[10, 11, 12], [13, 14, 15], [16, 17, 18]],
+    [[19, 20, 21], [22, 23, 24], [25, 26, 27]]
+])
+"""
+
+
+tensor = np.array([[[1,2],[2,1]],[[3,0],[4,3]]])
+print(find_derivative(tensor))
+#find_derivative(tensor)
