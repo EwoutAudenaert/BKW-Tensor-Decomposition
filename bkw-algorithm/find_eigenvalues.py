2e48ed89ca3bf9facab557b14809f2653a7ff7e7
EwoutAudenaert <84740105+EwoutAudenaert@users.noreply.github.com>
2025-04-04 11:44:50 +0200

    added complexity annotations

--- bkw-algorithm/find_eigenvalues.py
+++ bkw-algorithm/find_eigenvalues.py
@@ -1,10 +1,9 @@
-
 import numpy as np
 from scipy.linalg import inv, det
 
 def diagonalize_basis(basis, tol=1e-10):
-    results = []
-    for X_i in basis:
-        _,eigenvectors = np.linalg.eig(X_i)  
-        results.append(inv(eigenvectors.T)*det(eigenvectors))
-    return results
+    results = [] # time 1 ; space 1
+    for X_i in basis: # time m ; space 1
+        _,eigenvectors = np.linalg.eig(X_i) # time n^3 ; space n^2
+        results.append(inv(eigenvectors.T)*det(eigenvectors)) # time n^3 ; space n^2
+    return results # time 1 ; space m*n^2
