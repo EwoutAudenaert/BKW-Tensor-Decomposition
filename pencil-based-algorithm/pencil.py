03c9607eaffafd3aa276289e00707de6648d4777
EwoutAudenaert <84740105+EwoutAudenaert@users.noreply.github.com>
2025-04-15 15:53:38 +0200

    complexity

--- pencil-based-algorithm/pencil.py
+++ pencil-based-algorithm/pencil.py
@@ -8,20 +8,20 @@ from library import ttm
 
 def pencil_decompose(tensor):
     #todo take random linear combination of slices
-    n=len(tensor)
-    T1,T2 = tensor[:,:,0],tensor[:,:,1]
-    hiddenA = T1 @ inv(T2)
-    eigsA, A = np.linalg.eig(hiddenA)  # A
-    hiddenBt = inv(T1) @ T2 #B^-T diag(_) B^T with B a factor matrix
-    eigsB, Bt = np.linalg.eig(hiddenBt)  # B^T
-    Bt = Bt.conj()
-    B  = inv(Bt.T)
-    Ctensor = ttm(ttm(tensor,inv(A),1),inv(B),2)
+    n=len(tensor) # time O(1) ; space O(1)
+    T1,T2 = tensor[:,:,0],tensor[:,:,1] # time O(n^2) ; space O(n^2)
+    hiddenA = T1 @ inv(T2) # time O(n^3) ; space O(n^2)
+    eigsA, A = np.linalg.eig(hiddenA)  # time O(n^3) ; space O(n^2)
+    hiddenBt = inv(T1) @ T2 #B^-T diag(_) B^T with B a factor matrix ; time O(n^3) ; space O(n^2)
+    eigsB, Bt = np.linalg.eig(hiddenBt)  # B^T ; time O(n^3) ; space O(n^2)
+    Bt = Bt.conj() # time O(n^2) ; space O(1)
+    B  = inv(Bt.T) # time: O(n^3) ; space O(n^2)
+    Ctensor = ttm(ttm(tensor,inv(A),1),inv(B),2) # time O(n^4) ; space O(n^3)
     C = []
-    for i in range(n):
+    for i in range(n): # time O(n^2) ; space O(n^2)
         C.append(Ctensor[i,i,:])
     #we need to transpose because np puts the row into the columns
-    C = np.array(C).T.conj()
+    C = np.array(C).T.conj() # time O(n^2) ; space O(n^2)
     return [A,B,C]
 
 
